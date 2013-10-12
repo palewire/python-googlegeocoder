03d9b8a2ce170d31631ed640c8dc2c7199ee3549
palewire <ben.welsh@gmail.com>
2013-10-12 08:28:17 -0700

    PEP8 and pyflakes compliance and testing -- plus adding some files I forgot in the last commit.

--- googlegeocoder/__init__.py
+++ googlegeocoder/__init__.py
@@ -14,7 +14,7 @@ class GoogleGeocoder(object):
     A simple wrapper on version 3 of Google's geocoder API
     """
     BASE_URI = 'http://maps.googleapis.com/maps/api/geocode/json'
-    
+
     def _fetch_json(self, params):
         """
         Configure a HTTP request, fire it off and return the response.
@@ -61,7 +61,7 @@ class UnicodeMixin(object):
         def __str__(self):
             return self.__unicode__()
     # Python 2
-    else:  
+    else:
         def __str__(self):
             return self.__unicode__().encode('utf8')
 
@@ -72,7 +72,7 @@ class BaseAPIObject(UnicodeMixin):
     """
     def __init__(self, d):
         self.__dict__ = d
-    
+
     def __repr__(self):
         return '<%s: %s>' % (self.__class__.__name__, self.__str__())
 
@@ -80,31 +80,32 @@ class BaseAPIObject(UnicodeMixin):
 class GeocoderResult(BaseAPIObject):
     """
     A results objects returned by the API.
-    
+
     Contains the following attributes:
-    
-        formatted_address: A string containing the human-readable address of 
-            this location. Often this address is equivalent to the 
+
+        formatted_address: A string containing the human-readable address of
+            this location. Often this address is equivalent to the
             "postal address," which sometimes differs from country to country
-        
-        types: A list that indicates the type of the returned result. This 
-            array contains a set of one or more tags identifying the type of 
-            feature returned in the result. For example, a geocode of "Chicago" 
-            returns "locality" which indicates that "Chicago" is a city, and 
+
+        types: A list that indicates the type of the returned result. This
+            array contains a set of one or more tags identifying the type of
+            feature returned in the result. For example, a geocode of "Chicago"
+            returns "locality" which indicates that "Chicago" is a city, and
             also returns "political" which indicates it is a political entity.
-        
+
         address_components: A list of the different parts of the address
             as AddressComponent class objects
-        
+
         geometry: A collection of geometric data about the result, packaged
             as a Geoometry class object
-        
     """
     def __init__(self, d):
         self.__dict__ = d
-        self.address_components = [AddressComponent(i) for i in self.address_components]
+        self.address_components = [
+            AddressComponent(i) for i in self.address_components
+        ]
         self.geometry = Geometry(self.geometry)
-    
+
     def __unicode__(self):
         return six.u(self.formatted_address)
 
@@ -112,19 +113,18 @@ class GeocoderResult(BaseAPIObject):
 class AddressComponent(BaseAPIObject):
     """
     A piece of an address returned by the API
-    
+
     Contains the following attributes:
-        
-        long_name: The full text description or name of the address component 
+
+        long_name: The full text description or name of the address component
             as returned by the Geocoder.
-        
-        short_name: is an abbreviated textual name for the address component, 
+
+        short_name: is an abbreviated textual name for the address component,
             if available. For example, an address component for the state of
-             Alaska may have a long_name of "Alaska" and a short_name of "AK" 
+             Alaska may have a long_name of "Alaska" and a short_name of "AK"
             using the 2-letter postal abbreviation
-        
+
         type: A list indicating the type(s) of the address component.
-        
     """
     def __unicode__(self):
         return six.u(self.long_name)
@@ -133,22 +133,23 @@ class AddressComponent(BaseAPIObject):
 class Geometry(BaseAPIObject):
     """
     A collection of geometric data about a geocoder result.
-    
+
     Contains the following attributes:
-    
+
         location: The geocoded latitude and longitude as a Coordinate object.
-        
+
         location_type: Additional meta data about the location, could be:
             "ROOFTOP", "RANGE_INTERPOLATED", "GEOMETRIC_CENTER", "APPROXIMATE"
-        
+
         viewport: The recommended viewport for the returned result, returned as
             a Bounds class object.
-        
-        bounds: The bounding box that fully contains the result but may be bigger
-            than the recommended viewport.
-        
-        partial_match: Indicates that the geocoder did not return an exact 
-            match for the original request, though it did match part of the requested address
+
+        bounds: The bounding box that fully contains the result
+            but may be bigger than the recommended viewport.
+
+        partial_match: Indicates that the geocoder did not return an exact
+            match for the original request, though it did match part of the
+            requested address
     """
     def __init__(self, d):
         self.__dict__ = d
@@ -179,7 +180,7 @@ class Bounds(BaseAPIObject):
         self.__dict__ = d
         self.southwest = Coordinates(self.southwest)
         self.northeast = Coordinates(self.northeast)
-    
+
     def __unicode__(self):
         return six.u("(%s, %s)" % (self.southwest, self.northeast))
 
@@ -190,7 +191,7 @@ class Coordinates(BaseAPIObject):
     """
     def __unicode__(self):
         return six.u("(%s, %s)" % (self.lat, self.lng))
-    
+
     @property
     def wkt(self):
         return "POINT(%s %s)" % (self.lng, self.lat)
---
+++ test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from googlegeocoder import *
+from pprint import pprint
+
+
+class BaseTest(unittest.TestCase):
+
+    def setUp(self):
+        self.geocoder = GoogleGeocoder()
+
+class GoogleTest(BaseTest):
+
+    def test_address(self):
+        result = self.geocoder.get("Winnetka")
+        self.assertEqual(type(result[0]), GeocoderResult)
+
+    def test_latlng(self):
+        result = self.geocoder.get((34.236144,-118.500938))
+        self.assertEqual(len(result), 9)
+
+    def test_result_attributes(self):
+        result = self.geocoder.get("Winnetka")[0]
+        self.assertEqual(type(result.address_components), type([]))
+        [self.assertEqual(type(i), AddressComponent) for i in result.address_components]
+        [self.assertEqual(type(i.long_name), type(u"")) for i in result.address_components]
+        [self.assertEqual(type(i.short_name), type(u"")) for i in result.address_components]
+        [self.assertEqual(type(i.types), type([])) for i in result.address_components]
+        self.assertEqual(type(result.formatted_address), type(u""))
+        self.assertEqual(type(result.types), type([]))
+        self.assertEqual(type(result.geometry), Geometry)
+        self.assertEqual(type(result.geometry.location), Coordinates)
+        self.assertEqual(type(result.geometry.location_type), type(u""))
+        self.assertEqual(type(result.geometry.viewport), Bounds)
+        self.assertEqual(type(result.geometry.bounds), Bounds)
+        self.assertEqual(type(result.geometry.partial_match), type(True))
+        self.assertTrue(result.geometry.location.wkt.startswith('POINT(-87'))
+
+    def test_viewport_bias(self):
+        result = self.geocoder.get("Winnetka", 
+            bounding_box=((34.172684,-118.604794), (34.236144,-118.500938)))
+        self.assertEqual(result[0].formatted_address, 
+            u'Winnetka, Los Angeles, CA, USA')
+
+    def test_region_bias(self):
+        result = self.geocoder.get("Toledo", region='ES')
+        self.assertEqual(result[0].formatted_address, u'Toledo, Spain')
+
+    def test_language(self):
+        result = self.geocoder.get('Moscow', language='ru')
+        self.assertEqual(result[0].formatted_address, u'Москва, Россия')
+
+
+if __name__ == '__main__':
+    unittest.main()
