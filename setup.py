54d354a1938740110fdc16462d4b201fe186d5ef
palewire <ben.welsh@gmail.com>
2019-02-08 09:50:34 -0800

    v0.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='python-googlegeocoder',
-    version='0.3.0',
+    version='0.4.0',
     description='A simple Python wrapper for version three of Google\'s geocoder API',
     author='Ben Welsh',
     author_email='ben.welsh@latimes.com',
